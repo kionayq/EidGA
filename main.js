d637c046f5399c96d7a62ba8d904e42389644126
abinonayq <abinonayq@elm.sa>
2019-05-16 16:30:06 +0300

    download

--- main.js
+++ main.js
@@ -1,50 +1,62 @@
 var canvas = document.getElementById('viewport');
 context = canvas.getContext('2d');
 base_image = new Image();
-  base_image.src = 'base.jpg';
-  let db = firebase.firestore().collection('Eid');
-function make_base(){
+base_image.src = 'base.jpg';
+let db = firebase.firestore().collection('Eid');
+function make_base() {
 
     let nameI = document.getElementById('tt').value
-    db.add({
-        name: nameI});
+    if (nameI.length < 3 || nameI === undefined) {
 
-    document.getElementById('eid-Saeed').style.display = 'block';;
-  
-//   base_image.width= "100%";
-console.log(textInput)
-var textInput = document.getElementById('tt').value;
+        alert('أدخل اسمك رجاء : ) ');
+
+    } else {
 
 
-document.getElementById("demo").innerHTML = textInput;
+        db.add({
+            name: nameI});
+
+        document.getElementById('eid-Saeed').style.display = 'block';;
+        document.getElementById('aa').style.display = 'block';
+        //   base_image.width= "100%";
+        console.log(textInput)
+        var textInput = document.getElementById('tt').value;
 
 
-var base_imageH= base_image.height; 
-canvas.clientWidth = base_image.width ; 
-canvas.clientHeight = base_imageH; 
-
-  // var imgSize = calculateAspectRatioFit(base_image.width, base_image.height, canvas.clientWidth, canvas.clientHeight);
+        document.getElementById("demo").innerHTML = textInput;
 
 
-function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {  
-  // var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
-//   console.log(maxHeight)
-  var rtnWidth = srcWidth ;
-  var rtnHeight = srcHeight ;
-  return {
-      width: rtnWidth,
-      height: rtnHeight  
-  };
-//   maxWidth = 634;
+        var base_imageH = base_image.height;
+        canvas.clientWidth = base_image.width;
+        canvas.clientHeight = base_imageH;
+
+        // var imgSize = calculateAspectRatioFit(base_image.width, base_image.height, canvas.clientWidth, canvas.clientHeight);
+
+
+        function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+            // var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+            //   console.log(maxHeight)
+            var rtnWidth = srcWidth;
+            var rtnHeight = srcHeight;
+            return {
+                width: rtnWidth,
+                height: rtnHeight
+            };
+            //   maxWidth = 634;
+        }
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(base_image, 0, 0, canvas.width, canvas.height);
+        context.font = 'bold 18px Tajawal';
+        context.textAlign = 'end';
+        context.fillStyle = "#dc3545";
+        context.fillText(textInput, 40, 348);
+
+        document.getElementById("theimage").src = canvas.toDataURL();
+
+    }
 }
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(base_image, 0, 0 , canvas.width , canvas.height);
-    context.font = 'bold 18px Tajawal';
-    context.textAlign = 'end';
-    context.fillStyle = "#dc3545"; 
-    context.fillText(textInput, 40, 348);
-}
-// make_base();
+
 
 
 // File
@@ -52,35 +64,49 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 
 
 function handleFile(e) {
-  //Get the files from Upload control
-     var files = e.target.files;
-     var i, f;
-  //Loop through files
-     for (i = 0, f = files[i]; i != files.length; ++i) {
-         var reader = new FileReader();
-         var name = f.name;
-         reader.onload = function (e) {
-             var data = e.target.result;
+    //Get the files from Upload control
+    var files = e.target.files;
+    var i, f;
+    //Loop through files
+    for (i = 0, f = files[i]; i != files.length; ++i) {
+        var reader = new FileReader();
+        var name = f.name;
+        reader.onload = function (e) {
+            var data = e.target.result;
 
-             var result;
-             var workbook = XLSX.read(data, { type: 'binary' });
-             
-             var sheet_name_list = workbook.SheetNames;
-             sheet_name_list.forEach(function (y) { /* iterate through sheets */
-                 //Convert the cell value to Json
-                 var roa = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
-                 if (roa.length > 0) {
-                     result = roa;
-                 }
-             });
+            var result;
+            var workbook = XLSX.read(data, { type: 'binary' });
+
+            var sheet_name_list = workbook.SheetNames;
+            sheet_name_list.forEach(function (y) { /* iterate through sheets */
+                //Convert the cell value to Json
+                var roa = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+                if (roa.length > 0) {
+                    result = roa;
+                }
+            });
             //Get the result
-             console.log(result);
-         };
-         reader.readAsArrayBuffer(f);
-     }
- }
+            console.log(result);
+        };
+        reader.readAsArrayBuffer(f);
+    }
+}
+
+
+
+function to_image(e) {
+    var btn = document.getElementById("aa");
+    btn.addEventListener('click', function (e) {
+        var canvas = document.getElementById("viewport");
+        var dataURL = canvas.toDataURL();
+        btn.href = dataURL;
+    })
+
+}
+
+
 
 //Change event to dropdownlist
-$(document).ready(function(){
- $('#files').change(handleFile);
+$(document).ready(function () {
+    $('#files').change(handleFile);
 });
